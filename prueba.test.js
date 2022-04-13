4e1b40de4184d9c1df61c380c3dd2408eca22998
maicol torres <maicolt401@gmail.com>
2022-04-13 11:22:52 +0200

    integrate test to java

--- prueba.test.js
+++ prueba.test.js
@@ -11,14 +11,108 @@ const strictEquals = (a, b) => {
 
 describe("Given a strictEquals function", () => {
   describe("When it receives values a and b ", () => {
-    describe("Then it should return true", () => {
+    test("Then it should return true", () => {
       const a = 1;
       const b = 1;
+      const numberResult = true;
+
+      const numberTrue = strictEquals(a, b);
+
+      expect(numberTrue).toBe(numberResult);
+    });
+  });
+
+  describe("When it receives values NaN and NaN ", () => {
+    test("Then it should return false", () => {
+      const a = NaN;
+      const b = NaN;
+      const numberResult = false;
+
+      const isItTrue = strictEquals(a, b);
+
+      expect(isItTrue).toBe(numberResult);
+    });
+  });
+
+  describe("When it receives values 0 and -0 ", () => {
+    test("Then it should return true", () => {
+      const a = 0;
+      const b = -0;
       const numberResult = true;
 
       const isItTrue = strictEquals(a, b);
 
       expect(isItTrue).toBe(numberResult);
+    });
+  });
+
+  describe("When it receives values -0 and 0 ", () => {
+    test("Then it should return true", () => {
+      const a = -0;
+      const b = 0;
+      const numberResult = true;
+
+      const isItTrue = strictEquals(a, b);
+
+      expect(isItTrue).toBe(numberResult);
+    });
+  });
+
+  describe("When it receives values 1 and `1 `!", () => {
+    test("Then it should return true", () => {
+      const a = 1;
+      const b = "1";
+      const numberResult = false;
+
+      const isItTrue = strictEquals(a, b);
+
+      expect(isItTrue).toBe(numberResult);
+    });
+  });
+
+  describe("When it receives values true and false", () => {
+    test("Then it should return false", () => {
+      const value1 = 1;
+      const value2 = 1;
+      const numberResult2 = true;
+
+      const a = 1;
+      const b = "1";
+      const numberResult = false;
+
+      const isItflase = strictEquals(a, b);
+
+      expect(isItflase).toBe(numberResult, numberResult2);
+    });
+  });
+
+  describe("When it receives values true and false", () => {
+    test("Then it should return false", () => {
+      const a = 1;
+      const b = "1";
+      const numberResult = false;
+      const value1 = 1;
+      const value2 = "1";
+      const numberResult2 = false;
+
+      const isItflase = strictEquals(a, b);
+
+      expect(isItflase).toBe(numberResult, numberResult2);
+    });
+  });
+
+  describe("When it receives values `wate` and `oil` ", () => {
+    test("Then it should return false", () => {
+      const a = 1;
+      const b = "1";
+      const numberResult = false;
+      const value1 = 1;
+      const value2 = "1";
+      const numberResult2 = false;
+
+      const isItflase = strictEquals(a, b);
+
+      expect(isItflase).toBe(numberResult, numberResult2);
     });
   });
 });
